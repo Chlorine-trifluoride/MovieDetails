8b908079b98246913decd71554832a993333ec03
Chlorine-trifluoride <67011284+Chlorine-trifluoride@users.noreply.github.com>
2020-08-27 12:43:38 +0300

    Menus and movie search

---
+++ MovieCLI/MovieSearch.cs
@@ -0,0 +1,67 @@
+﻿using MovieLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieCLI
+{
+    class MovieSearch
+    {
+        private MovieController movieController;
+
+        public MovieSearch(MovieController movieController)
+        {
+            this.movieController = movieController;
+        }
+
+        public void Run()
+        {
+            PrintInstructions();
+            SearchForMovie();
+        }
+
+        private void PrintInstructions()
+        {
+            Console.Clear();
+            Console.WriteLine("Search for a movies by Title");
+        }
+
+        private void SearchForMovie()
+        {
+            string searchString = Console.ReadLine();
+            List<Movie> movies = OMDbAPI.SearchForMoviesByTitle(searchString);
+
+            for (int i = 0; i < movies.Count; i++)
+            {
+
+                Console.WriteLine($"{i}\t{movies[i].TitleYear}");
+            }
+
+            Console.WriteLine("============");
+            Console.WriteLine("Enter the ID of the movie you wish to load full info for.");
+
+            // Select the specific movie and get the info
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int selection)
+                && selection >= 0 && selection < movies.Count)
+            {
+                    LoadInfoForMovie(movies[selection]);
+            }
+
+            else
+                Console.WriteLine("Invalid input");
+        }
+
+        private void LoadInfoForMovie(Movie movie)
+        {
+            movie = OMDbAPI.GetMovieByImdbID(movie.IMDBID);
+            Console.WriteLine(movie.TitleYear);
+            Console.WriteLine(movie.Plot);
+            Console.WriteLine(movie.Metascore);
+
+            Console.WriteLine("=========");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
--- MovieCLI/Program.cs
+++ MovieCLI/Program.cs
@@ -6,9 +6,54 @@ namespace MovieCLI
 {
     class Program
     {
+        private static bool quit = false;
+        private static MovieController movieController;
+
         static void Main(string[] args)
         {
-            MovieController movieController = new MovieController();
+            movieController = new MovieController();
+
+            while (!quit)
+                PrintMenu();
+        }
+
+        static void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1) AddAndPrintDummyMovies");
+            Console.WriteLine("2) Search for a movie");
+            Console.WriteLine("Q) Quit program");
+
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        AddPrintDummyMovies();
+                        return;
+
+                    case ConsoleKey.D2:
+                        SearchForMovie();
+                        return;
+
+                    case ConsoleKey.Q:
+                    case ConsoleKey.E:
+                        quit = true;
+                        return;
+                }
+            }
+        }
+
+        static void SearchForMovie()
+        {
+            MovieSearch movieSearch = new MovieSearch(movieController);
+            movieSearch.Run();
+        }
+
+        static void AddPrintDummyMovies()
+        {
+            Console.Clear();
+
             movieController.AddCachedDummyMovies();
 
             List<Movie> cachedMovies = movieController.GetCachedMovies();
@@ -17,6 +62,9 @@ namespace MovieCLI
             {
                 Console.WriteLine(cachedMovies[i].IDTitle);
             }
+
+            Console.WriteLine("Press any key to...");
+            Console.ReadKey(true);
         }
     }
 }
