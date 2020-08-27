d1a583c1c5df2f725ac133f5ad7894bf81343993
Chlorine-trifluoride <67011284+Chlorine-trifluoride@users.noreply.github.com>
2020-08-27 11:05:28 +0300

    Movie data model

---
+++ MovieLib/Entities/Movie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MovieLib
+{
+    public class Movie
+    {
+        #region JSON
+        [Key]
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("Title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("Year")]
+        public string Year { get; set; }
+
+        [JsonPropertyName("Rated")]
+        public string Rated { get; set; }
+
+        [JsonPropertyName("Runtime")]
+        public string Runtime { get; set; }
+
+        [JsonPropertyName("Genre")]
+        public string Genre { get; set; }
+
+        [JsonPropertyName("Director")]
+        public string Director { get; set; }
+
+        [JsonPropertyName("Plot")]
+        public string Plot { get; set; }
+
+        [JsonPropertyName("Poster")]
+        public string PosterURL { get; set; }
+
+        // ratings?
+
+        [JsonPropertyName("Metascore")]
+        public int Metascore { get; set; }
+
+        [JsonPropertyName("imdbID")]
+        public int IMDBID { get; set; }
+
+        [JsonPropertyName("BoxOffice")]
+        public string BoxOffice { get; set; }
+
+        [JsonPropertyName("Response")]
+        public bool Response { get; set; }
+        #endregion
+
+        public string TitleYear => $"{Title} ({Year})";
+    }
+}
