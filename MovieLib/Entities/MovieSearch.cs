b8a01cb34dd66a872b408d9d89e25094357ca308
Chlorine-trifluoride <67011284+Chlorine-trifluoride@users.noreply.github.com>
2020-08-27 12:42:38 +0300

    Movie search data model for omdbapi

---
+++ MovieLib/Entities/MovieSearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MovieLib
+{
+    public class MovieSearch
+    {
+        [JsonPropertyName("Search")]
+        public List<Movie> ResultMovies { get; set; }
+    }
+}
