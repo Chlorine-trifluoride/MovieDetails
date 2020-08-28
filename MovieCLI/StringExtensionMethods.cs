f03a63c59ee214d64da2612669453b4565443d2e
Mikael N <uusikauttaja02022020@outlook.com>
2020-08-28 15:43:54 +0300

    String extension method StripDiacritics

---
+++ MovieCLI/StringExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Encodings;
+
+namespace MovieCLI
+{
+    static class StringExtensionMethods
+    {
+        /// <summary>
+        /// This strips all special diacritics and accents from strings.
+        /// Very useful for searching foreign movies
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>string without diacritics</returns>
+        public static string StripDiacritics(this string str)
+        {
+            string normalized = str.Normalize(NormalizationForm.FormD);
+            StringBuilder builder = new StringBuilder();
+
+            foreach (char c in normalized)
+            {
+                var v = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (v != UnicodeCategory.NonSpacingMark)
+                    builder.Append(c);
+            }
+
+            return builder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
